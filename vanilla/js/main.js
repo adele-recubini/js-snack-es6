c2cce07ed74eb73cd758542a3f5dbc709725bca4
adele <adele.r87@gmail.com>
2021-01-19 11:42:28 +0100

    modifiche

--- vanilla/js/main.js
+++ vanilla/js/main.js
@@ -1,1 +1,52 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore
 
+
+ var biciclette = [
+    {
+      'nome': 'bianchi',
+      'peso': 15,
+
+   },
+
+
+    {
+    'nome': 'masciarelli',
+    'peso': 8,
+
+  },
+
+  {
+    'nome': 'rossi',
+    'peso': 20,
+
+ },
+
+];
+
+// per capire quale bici pesa di meno dobbiamo comprarare gli oggetti all interno dell array . io vedrò la prima bicicletta e per me quella sara da comparare con le altre quindi bicicletta leggera sara la mia bianchi  e poi prendo la proprietà peso e la comparo
+
+var biciLeggera = biciclette[0]
+for (var i = 0; i < biciclette.length; i++) {
+  if (biciclette[i].peso <  biciLeggera.peso) {
+    biciLeggera = biciclette[i]
+  }
+}
+console.log(biciLeggera);
+
+
+
+// var pesata = 8
+// for (var i = 0; i < biciclette.length; i++) {
+//
+//    for (var key in biciclette) {
+//      console.log(biciclette[i]['peso']);
+//      var pesi = biciclette[i]
+//    }
+//    console.log(pesi.peso);
+//    if (pesi.peso === pesata) {
+//     console.log(pesi.peso);
+//    }
+
+// }
